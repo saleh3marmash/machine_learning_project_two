61894bb3b02f85872f8a5adc1ed4b841106556ce
saleh3marmash <1193300@student.birzeit.edu>
2024-01-25 21:38:04 +0200

    adding chef model ready

---
+++ saleh_majed_project2/outputs/rules/rules.py
@@ -0,0 +1,1 @@
+def findDecision(obj): #obj[0]: Age, obj[1]: Gender, obj[2]: BMI, obj[3]: Region, obj[4]: No. Childred, obj[5]: Insurance Charges, obj[6]: age_bins, obj[7]: bmi_bins, obj[8]: insurance_bins, obj[9]: children_bins
